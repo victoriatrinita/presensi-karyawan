2166a97493bacd39870708a48cfd2259a0b01773
victoriatrinita <victoriatrinita@yahoo.com>
2020-11-29 03:26:20 +0700

    chore: set heroku deployment configuration

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const { port } = require("./config");
 const presensisRoutes = require("./routes/presensis");
+const path = require("path");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -23,6 +24,12 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use("/api/presensis", presensisRoutes);
-app.get("/", (req, res) => res.send("hello world"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/public"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log("Express is running on port " + port));
