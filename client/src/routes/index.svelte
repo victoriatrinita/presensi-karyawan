bf3964725547ae44642d645217216380b6d29dcc
victoriatrinita <victoriatrinita@yahoo.com>
2020-11-29 02:57:44 +0700

    fix: add router file

---
+++ client/src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { Router, Route } from 'svelte-routing';
+	import Index from '../routes/pages/index.svelte';
+	import About from '../routes/pages/about.svelte';
+	import Blog from '../routes/pages/blog.svelte';
+
+	export let url = '';
+</script>
+
+<Router {url}>
+	<div>
+		<Route path="blog" component={Blog} />
+		<Route path="about" component={About} />
+		<Route path="/" component={Index} />
+	</div>
+</Router>
